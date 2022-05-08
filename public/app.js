e962359b91b83ad8620004e3308e64caadfc6b23
Pardjai <Ilia.Kozionov@yandex.ru>
2022-05-08 14:21:59 +0300

    added fetch(post) for addTask

--- public/app.js
+++ public/app.js
@@ -14,13 +14,23 @@ new Vue({
         if (!title) {
           return
         }
-        this.todos.push({
-          title: title,
-          id: Math.random(),
-          done: false,
-          date: new Date()
+
+        fetch('/api/todo', {
+          method: 'POST',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            title,
+            done: false,
+          })
         })
-        this.todoTitle = ''
+        .then(res => res.json())
+        .then (({todo}) => {
+          console.log(todo)
+
+          this.todos.push(todo)
+          this.todoTitle = ''
+        })
+        .catch(e => console.log(e))
       },
       removeTodo(id) {
         this.todos = this.todos.filter(t => t.id !== id)
@@ -30,12 +40,20 @@ new Vue({
       capitalize(value) {
         return value.toString().charAt(0).toUpperCase() + value.slice(1)
       },
-      date(value) {
-        return new Intl.DateTimeFormat('ru-RU', {
+      date(value, withTime) {
+        const options = {
           year: 'numeric',
           month: 'long',
           day: '2-digit'
-        }).format(new Date(value))
+        }
+
+        if (withTime) {
+          options.hour = '2-digit'
+          options.minute = '2-digit'
+          options.second = '2-digit'
+        }
+
+        return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
       }
     }
   })
--- routes/todo.js
+++ routes/todo.js
@@ -4,22 +4,56 @@ const router = Router()
 
 // Получение списка задач
 router.get('/', (req, res) => {
-    
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 // Добавление задачи
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    try {
+        console.log(req);
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false,
+        })
 
+        res.status(201).json({todo})
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 // Изменение статуса задачи
 router.put('/:id', (req, res) => {
-
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 // Удаление задачи
 router.delete('/:id', (req, res) => {
-
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 module.exports = router
